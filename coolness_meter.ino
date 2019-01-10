412a9afce210c827dc6159bb0caf8e66912fc6da
sstroe <32340296+sstroe@users.noreply.github.com>
2019-01-10 13:06:47 -0500

    The final code

--- coolness_meter.ino
+++ coolness_meter.ino
@@ -10,84 +10,49 @@ void setup() {
  pinMode(6, OUTPUT);
  pinMode(5, OUTPUT);
  pinMode(4, OUTPUT);
- 
- 
 }
 
 void loop() {
-int stan = analogRead(0);
-
-Serial.println(stan);
-
+  // put your main code here, to run repeatedly:
+  int stan = analogRead(0);
+  Serial.println(stan);
+  if (stan > 100) {
+   blink();
+   for(int i = 13; i > random(4,13); i--) {
+    digitalWrite(i, HIGH);
+    delay(1000);
+   }
+   delay(2000);
+   blink();
+   turnLightsOff();
+  }
  
-if (stan > 150 && stan < 1000) {
-  digitalWrite(13, HIGH);
-}
-else {
-  digitalWrite(13, LOW);
-}
-if (stan > 355 && stan < 1000) {
-  digitalWrite(12, HIGH);
-}
-else {
-  digitalWrite(12, LOW);
-}
-if (stan > 459 && stan < 1000) {
-  digitalWrite(11, HIGH);
-}
-else {
-  digitalWrite(11, LOW);
-}
-if (stan > 560 && stan < 900) {
-  digitalWrite(10, HIGH);
-}
-else {
-  digitalWrite(10, LOW);
-}
-if (stan > 674 && stan < 900 ) {
-  digitalWrite(9, HIGH);
-}
-else {
-  digitalWrite(9, LOW);
-}
- 
-if (stan > 700 && stan < 890) {
-  digitalWrite(8, HIGH);
-}
-else {
-  digitalWrite(8, LOW);
 }
 
- if (stan > 770 && stan < 710) {
-  digitalWrite(7, HIGH);
-}
-else {
-  digitalWrite(7, LOW);
-}
- 
- if (stan > 50 && stan < 890) {
-  digitalWrite(6, HIGH);
-}
-else {
-  digitalWrite(6, LOW);
-}
-
- 
-if (stan > 710 && stan < 810) {
-  digitalWrite(5, HIGH);
-}
-else {
-  digitalWrite(5, LOW);
+void blink(){
+  turnLightsOn();
+  delay(200);
+  turnLightsOff();
+  delay(200);
+    turnLightsOn();
+  delay(200);
+  turnLightsOff();
+  delay(200);
+    turnLightsOn();
+  delay(200);
+  turnLightsOff();
+  delay(200);
+  
 }
 
-if (stan > 990 && stan < 100) {
-  digitalWrite(4, HIGH);
+void turnLightsOn() {
+  for (int i = 4; i < 14; i++) {
+    digitalWrite(i, HIGH);
+  }
 }
-else {
-  digitalWrite(4, LOW);
-}
 
-
-
-
+void turnLightsOff() {
+  for (int i = 4; i < 14; i++) {
+    digitalWrite(i, LOW);
+  }
 }
